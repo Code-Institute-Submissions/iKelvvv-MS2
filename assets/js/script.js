c648ce9a6ea0141958bfb451ca7c764e8b80e757
Kelvin Lee <kelvinlee@live.co.uk>
2021-07-16 12:58:56 +0100

    Add weapon functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,38 @@
 var userChoice;
+var computerChoice;
+var roundWinner;
 
 function selectRock() {
-
+    userChoice = 'Rock';
+    computerChoice = computerGenerator();
+    roundWinner = calculateWinner(userChoice, computerChoice);
 }
 
 function selectPaper() {
-
+    userChoice = 'Paper';
+    computerChoice = computerGenerator();
+    roundWinner = calculateWinner(userChoice, computerChoice);
 }
 
 function selectScissors() {
-
+    userChoice = 'Scissors';
+    computerChoice = computerGenerator();
+    roundWinner = calculateWinner(userChoice, computerChoice);
 }
 
 function selectLizard() {
-
+    userChoice = 'Lizard';
+    computerChoice = computerGenerator();
+    roundWinner = calculateWinner(userChoice, computerChoice);
 }
 
 function selectSpock() {
-
+    userChoice = 'Spock';
+    computerChoice = computerGenerator();
+    roundWinner = calculateWinner(userChoice, computerChoice);
 }
 
-function computerChoice() {
+function computerGenerator() {
 
 }
 
@@ -37,5 +49,5 @@ function firstToThree() {
 }
 
 function scoreBoard() {
-    
+
 }
