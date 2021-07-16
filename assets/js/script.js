e920c1dffc7297c1501bfc8f52b76f893507d5ff
Kelvin Lee <kelvinlee@live.co.uk>
2021-07-16 12:54:00 +0100

    Add basic functions for game

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+var userChoice;
+
+function selectRock() {
+
+}
+
+function selectPaper() {
+
+}
+
+function selectScissors() {
+
+}
+
+function selectLizard() {
+
+}
+
+function selectSpock() {
+
+}
+
+function computerChoice() {
+
+}
+
+function calculateWinner() {
+
+}
+
+function resetGame() {
+
+}
+
+function firstToThree() {
+
+}
+
+function scoreBoard() {
+    
+}
