d61fd239ff737c0d69272908f3dc518ca5119e52
Kelvin Lee <kelvinlee@live.co.uk>
2021-07-16 13:08:57 +0100

    Add computer generator function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const computerChoiceDisplay = document.getElementById("computer-choice");
+
 var userChoice;
 var computerChoice;
 var roundWinner;
@@ -6,6 +8,7 @@ function selectRock() {
     userChoice = 'Rock';
     computerChoice = computerGenerator();
     roundWinner = calculateWinner(userChoice, computerChoice);
+    
 }
 
 function selectPaper() {
@@ -33,7 +36,25 @@ function selectSpock() {
 }
 
 function computerGenerator() {
+    var randomNumber = Math.floor(Math.random() * 5) + 1;
 
+    if (randomNumber === 1) {
+        computerChoice = "Rock";
+    }
+    if (randomNumber === 2) {
+        computerChoice = "Paper";
+    }
+    if (randomNumber === 3) {
+        computerChoice = "Scissors";
+    }
+    if (randomNumber === 4) {
+        computerChoice = "Lizard";
+    }
+    if (randomNumber === 5) {
+        computerChoice = "Spock";
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+    return computerChoice;
 }
 
 function calculateWinner() {
